18d6f8b24f158c485922d22d2e2f1e083c2b2ade
pnguy118 <phucnhat2111@gmail.com>
2024-05-24 20:57:09 -0400

    add Google Search Control meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="min-h-full">
+            <head>
+                <meta
+                    name="google-site-verification"
+                    content="pwlGFMeXIMD-gryNsZX6ieMokC86VTiNwvguXzFMZKA"
+                />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </head>
             <body className={`${inter.className} min-h-screen`}>
                 <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
             </body>
